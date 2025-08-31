0ff3afef2efd5ddfeb7d26099448832a593bcd62
Apurv445 <sharmabipin0951@gmail.com>
2025-08-31 19:46:43 +0530

    Add project file

---
+++ controllers/users.js
@@ -0,0 +1,44 @@
+const User = require("../models/user");
+
+module.exports.renderSignupForm = (req, res) => {
+  res.render("users/signup.ejs");
+};
+
+module.exports.signup = async (req, res) => {
+  try {
+    let { username, email, password } = req.body;
+    const newUSer = new User({ email, username });
+    const registeredUser = await User.register(newUSer, password);
+    console.log(registeredUser);
+    req.login(registeredUser, (err) => {
+      if (err) {
+        return next(err);
+      }
+      req.flash("success", "Welcome To Wanderlust");
+      res.redirect("/listings");
+    });
+  } catch (e) {
+    req.flash("error", e.message);
+    res.redirect("/signup");
+  }
+};
+
+module.exports.renderLoginForm = (req, res) => {
+  res.render("users/login.ejs");
+};
+
+module.exports.login = async (req, res) => {
+  req.flash("success", "Welcome Back To Wanderlust");
+  let redirecturl = res.locals.redirectUrl || "/listings";
+  res.redirect(redirecturl);
+};
+
+module.exports.logout = (req, res) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    req.flash("success", "You are logged out!");
+    res.redirect("/listings");
+  });
+};
